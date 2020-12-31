59eac88cc643437278ca8b38c35e2027cebbf145
Andrii <butsaandrii@gmail.com>
2020-12-31 06:53:33 +0200

    I add timer to page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded",() => { /* js чекає загрузки DOM дерева */ 
 
+    // tabs | Выберите стиль питания |
     const tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent =document.querySelectorAll('.tabcontent'),
     tabsParent = document.querySelector('.tabheader__items')
@@ -36,4 +37,60 @@ document.addEventListener("DOMContentLoaded",() => { /* js чекає загру
             })
         }
     })
+
+    // ------------------------------------------------------------
+
+    // Timer  |Осталось до конца акции: |
+
+    const deadline = '2021-01-12'
+    function getTimeRemaining (endtime) { /* Отримує різницю між датами */
+        const t = Date.parse(deadline) - new Date(),
+              days = Math.floor(t/(1000 * 60 *60 *24)),  /* floor - округлює до найближчого цілого */
+              hours = Math.floor((t/( 1000 * 60 * 60 )% 24 )),
+              minutes = Math.floor((t/ 1000 / 60 ) % 60 ),
+              seconds = Math.floor((t / 1000 ) % 60 );
+
+        return {
+            'total' : t,
+            'days' : days,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock (selector, endtime) { /* встановлюєм таймер на нашу сторінку */
+        const timer =document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds');
+        
+              updateClock() /* викликаєм функцію,щоб значення зразу запсувались на сторінку */ 
+
+        function getZero(num) {
+            if (num >= 0 && num <= 9) {
+                return `0${num}`
+            }else {return num;}
+        }
+
+        function updateClock () { /* перезапис значень годинника */ 
+            const t = getTimeRemaining(endtime) 
+                
+            days.innerHTML = getZero(t.days)
+            hours.innerHTML = getZero(t.hours)
+            minutes.innerHTML = getZero(t.minutes)
+            seconds.innerHTML = getZero(t.seconds)
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval)
+            }
+            
+        } 
+
+        const timeInterval = setInterval(updateClock,1000) /* Годинник працює */ 
+    } 
+
+    setClock('.timer',deadline)
+    // ------------------------------------------------------------
 })
