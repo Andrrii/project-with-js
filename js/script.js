4cf706ca1d2d43e7a0ac8c78c6818531d27cccb0
Andrii <butsaandrii@gmail.com>
2021-01-05 04:04:22 +0200

    Добавив json-server

--- js/script.js
+++ js/script.js
@@ -256,17 +256,23 @@ document.addEventListener("DOMContentLoaded",() => { /* js чекає загру
                 margin: 0 auto;
             `
             form.insertAdjacentElement('afterend',statusMessage) // ставим після форми
-            const request = new XMLHttpRequest()   
+            
+            // XMLHttpRequest
+            //#region 
+            /*const request = new XMLHttpRequest()   
 
-            request.open('POST', 'server.php')
+            request.open('POST', 'server.php')*/
 
             /* якщо ми юзаєм клас XMLHttpRequest то  |request.setRequestHeader('Content-type','multipart/form-data')| не потрібний!!!!!!!! */
             // request.setRequestHeader('Content-type','multipart/form-data')
             //-----------------------------------
-            const formData = new FormData(form) // Отримуєм дані з форми з допомогою класу  FormData 
+            //const formData = new FormData(form) // Отримуєм дані з форми з допомогою класу  FormData 
             // in HTML завжди повинен бути тег name
-            request.send(formData) // Відправляєм дані на сервер
+            //request.send(formData) // Відправляєм дані на сервер
+            //#endregion
+            
             // Для JSON відправки
+            //#region 
             /*const request = new XMLHttpRequest()   
 
             request.open('POST', 'server.php')
@@ -279,18 +285,47 @@ document.addEventListener("DOMContentLoaded",() => { /* js чекає загру
             })
             request.send(JSON.stringify(object)) */  // Відправляєм дані на сервер
             //-----------------------------------------------
-            
+            //#endregion
 
-            request.addEventListener('load', (event) => {
-                if (request.status === 200) {
-                    console.log(request.response)
-                    showThanksModal(message.success)
-                    form.reset()
-                    statusMessage.remove()
-                  
-                }else{showThanksModal(message.fail)
-                }
+            const formData = new FormData(form) // Отримуєм дані з форми з допомогою класу  FormData 
+
+            // Fetch API // Використовує проміси // Для роботи із сервером
+            //  зручніший ніж XMLHttpRequest
+
+            const object = {} // для перетворення formData в JSON
+            formData.forEach(function(value,key){
+                object[key] = value // для перетворення formData в JSON
             })
+            fetch('server.php', {
+                method: 'POST',
+                 headers: {
+                     'Content-type':'application/json'
+                 },
+                body: JSON.stringify(object)
+            }).then(data => data.text())
+            .then(data => {
+                console.log(data)
+                showThanksModal(message.success)
+                statusMessage.remove()
+            }).catch( () => {
+                showThanksModal(message.fail)
+            }).finally(() => {
+                form.reset()
+             })
+
+            // EventListener('load') для JSON and XMLHttpRequest
+            //#region 
+            // request.addEventListener('load', (event) => {
+            //     if (request.status === 200) {
+            //         console.log(request.response)
+            //         showThanksModal(message.success)
+            //         form.reset()
+            //         statusMessage.remove()
+                  
+            //     }else{showThanksModal(message.fail)
+            //     }
+            // })
+            //#endregion
 
         })
 
@@ -322,5 +357,15 @@ document.addEventListener("DOMContentLoaded",() => { /* js чекає загру
     }
  
 // -----------------------------------------------------------
+
+// json-server -- емуляція backend + встановив npm packet
+// see documentation on git
+
+    fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res))
+
+//-------------------------------------------------------
+
 
 })
--- server.php
+++ server.php
@@ -1,4 +1,4 @@
 <?php
-//$_POST = json_decode(file_get_contents("php://input"),true) --- Для роботи з JSON
+$_POST = json_decode(file_get_contents("php://input"),true); //--- Для роботи з JSON
 echo var_dump($_POST); /* бере дані Post запиту,перетворює
  їх в стрічку і показує на клієнті */
